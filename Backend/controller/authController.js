a8a7d13b4438738fcba93bb5364df4596d63bbbb
Maybemarc <marcspectre777@gmail.com>
2025-03-07 12:08:30 +0530

    fixing

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -77,7 +77,7 @@ export const loginUser = async (req, res) => {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "Lax",
       path: "/",
     });
